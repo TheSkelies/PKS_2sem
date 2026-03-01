a4eca6908ab7c8a1d5ef271fa027f096be3de560
TheSkelies <The_Skelies@mail.ru>
2026-03-01 13:35:25 +0300

    first commit

---
+++ KR1/hello.cpp
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+void printHelloWorld(std::ostream& console) {
+    console << "Hello, World!\n";
+}
---
+++ KR1/main.cpp
@@ -0,0 +1,5 @@
+#include "hello.cpp"
+
+int main() {
+    printHelloWorld(std::cout);
+}
